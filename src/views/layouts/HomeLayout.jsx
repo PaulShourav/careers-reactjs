e15a534d696a461b0fe65e235ac92a8aa34bca09
Shourav paul <paul.shourav01@gmail.com>
2023-10-16 00:28:36 +0600

    Updated HomeLayout

--- src/views/layouts/HomeLayout.jsx
+++ src/views/layouts/HomeLayout.jsx
@@ -1,9 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../../components/homeComponents/Navbar";
 import Footer from "../../components/homeComponents/Footer";
+import useAuth from "../../hooks/useAuth";
+import Loading from "../../components/common/Loading";
 
 
 const HomeLayout = () => {
+    const {user}=useAuth()
+    if (!user) {
+        return <Loading/>
+    }
     return (
         <>
         <Navbar/>
