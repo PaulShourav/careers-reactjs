57881828b08211e12b76b4495bee2745c928f3f2
Shourav paul <paul.shourav01@gmail.com>
2023-11-16 22:50:39 +0600

    Dashboard navbar

--- src/views/layouts/DashboardLayout.jsx
+++ src/views/layouts/DashboardLayout.jsx
@@ -1,30 +1,47 @@
-import { Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 import { Link } from 'react-router-dom';
 
 const DashboardLayout = () => {
     return (
-        <div className="drawer lg:drawer-open">
-                <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-                <div className="drawer-content flex flex-col ">
-                    {/* Page content here */}
-                    {/* <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label> */}
-                    {/* <Header/> */}
-                    <div className="px-10 py-12">
-
-                    <Outlet/>
+        <div className="drawer lg:drawer-open min-h-screen">
+            <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+            <div className="drawer-content flex flex-col ">
+                {/* Page content here */}
+                {/* <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label> */}
+                <div className="navbar bg-base-100 md:hidden">
+                    <div className="flex-none">
+                        <label htmlFor="my-drawer-2" className="btn btn-square btn-ghost drawer-button">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+                        </label>
                     </div>
+                    <div className="flex-1">
+                    <p className=" text-xl  font-bold"><span className="text-green-500">BD</span>-TECH SOLUATION</p>
+                    </div>
+                    {/* <div className="flex-none">
+                        <button className="btn btn-square btn-ghost">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
+                        </button>
+                    </div> */}
                 </div>
-                <div className="drawer-side">
-                    <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
-                    <ul className="menu p-4 w-80 h-full bg-base-200 text-base-content">
-                        {/* Sidebar content here */}
-                        <li><Link to={'/'}></Link></li>
-                        <li><Link to={'/dashboard/users'}>Users</Link></li>
-                        <li><Link to={'/dashboard/jobs'}>Jobs</Link></li>
-                    </ul>
+                <div className="px-10 py-12">
 
+                    <Outlet />
                 </div>
             </div>
+            <div className="drawer-side">
+                <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+                <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+                    {/* Sidebar content here */}
+                    <li>
+                        <p></p>
+                    </li>
+                    <li><Link to={'/'}></Link></li>
+                    <li><NavLink to={'/dashboard/users'} className={({ isActive }) => isActive ? "sidebar-active-link" : ""}>Users</NavLink></li>
+                    <li><NavLink to={'/dashboard/jobs'} className={({ isActive }) => isActive ? "sidebar-active-link" : ""}>Jobs</NavLink></li>
+                </ul>
+
+            </div>
+        </div>
     );
 };
 
